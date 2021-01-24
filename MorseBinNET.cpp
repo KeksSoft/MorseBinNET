78750b12147b8f438c09b4af6f51e50591d193de
RegenJoho <63968870+RegenJoho@users.noreply.github.com>
2021-01-24 15:58:03 +0100

    Stable version
    First stable version of MorseBinNet.

---
+++ MorseBinNET.cpp
@@ -0,0 +1,42 @@
+#include "Arduino.h"
+#include "MorseBinNET.h"
+#include "MorseBin.h"
+MorseBinNET::MorseBinNET(String address, int pinTransmit,int pinReceive){
+	_pinTransmit = pinTransmit;
+	_pinReceive = pinReceive;
+	_address = address;
+	pinMode(_pinTransmit,OUTPUT);
+	pinMode(_pinReceive,INPUT);
+}
+void MorseBinNET::send(String address, String data1, String data2){
+	MBSendByte(address,_pinTransmit);
+	MBSendByte(_address,_pinTransmit);
+	MBSendByte(data1,_pinTransmit);
+	MBSendByte(data2,_pinTransmit);	
+}
+String MorseBinNET::receive(){
+	String receivedOutput = "";
+	String incomingAddress = MBReceive(_pinReceive);
+	if(incomingAddress=="errTimeout"){
+		return "errTimeout";
+	}
+	String senderAddress = MBReceive(_pinReceive);
+	if(senderAddress=="errTimeout"){
+		return "errTimeout";
+	}
+	String data1 = MBReceive(_pinReceive);
+	if(data1=="errTimeout"){
+		return "errTimeout";
+	}
+	String data2 = MBReceive(_pinReceive);
+	if(data2=="errTimeout"){
+		return "errTimeout";
+	}
+	if (incomingAddress==_address){
+		receivedOutput = senderAddress + data1 + data2;
+		return receivedOutput;
+	}
+	else{
+		return "errWrongAddress";
+	}
+}
--- MorseBinNET.h
+++ MorseBinNET.h
@@ -1,5 +1,21 @@
 #ifndef MorseBinNET_h
 #define MorseBinNET_h
 #include "Arduino.h"
+<<<<<<< Updated upstream
+=======
+#include "MorseBin.h"
+	class MorseBinNET {
+		public:
+			MorseBinNET(String address, int pinTransmit,int pinReceive);
+			void send(String address, String data1, String data2);
+			String receive();
+			//String placeRequest(String address, String data1, String data2);
+			//String answerRequest(String data1, String data2)
+		private:
+			String _address;
+			int _pinTransmit;
+			int _pinReceive;
+	};
+>>>>>>> Stashed changes
 
 #endif
